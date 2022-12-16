bf84b9675c02a569b0e6ab073f358f72b1dea50b
Spinny <t.ayoub.93@gmail.com>
2022-12-16 23:27:38 +0100

    Completed the modules learning project

---
+++ practice-for-week-04-common-js-modules-main/index.js
@@ -0,0 +1,12 @@
+const sayHelloTo = require('./send-messages/say-hello-to');
+const giveMessageToMrsPotato = require('./send-messages/give-message-to-mrs-potato');
+const {message1,message2,message3} = require('./messages/index.js');
+
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+sayHelloTo("Mr. Potato");
+giveMessageToMrsPotato(message1);
+giveMessageToMrsPotato(message2);
+giveMessageToMrsPotato(message3);
